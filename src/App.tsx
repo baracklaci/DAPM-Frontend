b3e8afd8bbaa4dd8148c847bd6a244ff1b702230
Laszlo Barak <baracklaci@gmail.com>
2024-03-18 13:35:21 +0100

    Typescript example added

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,11 @@
-import React, { useCallback } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ReactFlow, { Background, BackgroundVariant, Connection, Controls, Edge, Handle, Position, addEdge, useEdgesState, useNodesState } from 'reactflow';
-import 'reactflow/dist/style.css';
-import styled from 'styled-components';
+import Flow from "./Flow";
 
+import "./index.css";
 
-const initialNodes = [
-  {
-    id: '1',
-    type: 'input',
-    data: { label: 'input node' },
-    position: { x: 250, y: 5 },
-  },
-];
-
-const CustomInput = () => (
-  <>
-    <div>Only connectable with B</div>
-    <Handle type="source" position={Position.Right} />
-  </>
-);
-
-const CustomNode = ({ id }:{id:any}) => (
-  <>
-    <Handle type="target" position={Position.Left} />
-    <div>{id}</div>
-    <Handle type="source" position={Position.Right} />
-  </>
-);
-
-const nodeTypes = {
-  custominput: CustomInput,
-  customnode: CustomNode,
-};
-
-const ValidationFlow = () => {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-
-  const onConnect = useCallback((params: Edge | Connection) => setEdges((els) => addEdge(params, els)), []);
-
-const ReactFlowStyled = styled(ReactFlow)`
-  background-color: #3b3b3b;
-`;
-
+export default function App() {
   return (
-    <div style={{ height: '100%' }}>
-      <ReactFlowStyled 
-      nodes={nodes}
-      edges={edges}
-      //onNodesChange={onNodesChange}
-      //onEdgesChange={onEdgesChange}
-      //onConnect={onConnect}
-      //selectNodesOnDrag={false}
-      //className="validationflow"
-      //nodeTypes={nodeTypes}
-      >
-      <Background
-        color="#d9d9d9"
-        variant={BackgroundVariant.Dots}
-      />
-        <Controls />
-      </ReactFlowStyled>
+    <div className="App">
+      <Flow />
     </div>
   );
 }
-
-export default ValidationFlow;
