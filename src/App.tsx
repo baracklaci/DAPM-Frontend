c6ee5e3b8a21616ac820441b6e4152fc70f7bbed
Laszlo Barak <baracklaci@gmail.com>
2024-03-27 17:28:22 +0100

    Added redux persist, so changes are not lost

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,18 @@ import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./redux/slices";
 
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { PersistGate } from "redux-persist/integration/react";
+
+// Configure redux-persist
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer<ReturnType<typeof rootReducer>>(persistConfig, rootReducer);
+
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
@@ -16,26 +28,30 @@ const darkTheme = createTheme({
 });
 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
 })
+
+export const persistor = persistStore(store);
 
 export default function App() {
   return (
     <ThemeProvider theme={darkTheme}>
-    <div className="App">
-    <Provider store={store}>
-      <ReactFlowProvider>
-        <Flow />
-        <Box sx={{ display: 'flex' }}>
-          <PipelineAppBar />
-          <Box sx={{ display: 'flex', flexDirection: 'column'}}>
-            <Sidebar />
-            <Controls style={{position: 'fixed', bottom: '0px', left: '240px'}} />
-          </Box>
-        </Box>
-      </ReactFlowProvider>
-    </Provider>
-    </div>
+      <div className="App">
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <ReactFlowProvider>
+              <Flow />
+              <Box sx={{ display: 'flex' }}>
+                <PipelineAppBar />
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                  <Sidebar />
+                  <Controls style={{ position: 'fixed', bottom: '0px', left: '240px' }} />
+                </Box>
+              </Box>
+            </ReactFlowProvider>
+          </PersistGate>
+        </Provider>
+      </div>
     </ThemeProvider>
   );
 }
