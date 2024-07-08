628c1d93d046d7e4b62ea7060630e1271d01280d
Laszlo Barak <baracklaci@gmail.com>
2024-07-08 15:09:32 +0200

    pipeline deployment fix

--- src/components/PipeLineComposer/PipelineAppBar.tsx
+++ src/components/PipeLineComposer/PipelineAppBar.tsx
@@ -3,15 +3,14 @@ import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getActiveFlowData, getActivePipeline } from "../../redux/selectors";
-import React, { useState } from "react";
-import { setImageData, updatePipelineName } from "../../redux/slices/pipelineSlice";
+import { useState } from "react";
+import { updatePipelineName } from "../../redux/slices/pipelineSlice";
 import EditIcon from '@mui/icons-material/Edit';
-import { Node, getRectOfNodes, getTransformForBounds, getViewportForBounds } from "reactflow";
-import { toPng } from "html-to-image";
-import { DataSinkNodeData, DataSourceNodeData, NodeData, OperatorNodeData } from "../../redux/states/pipelineState";
-import { putPipeline } from "../../services/backendAPI";
+import { Node } from "reactflow";
+import { DataSinkNodeData, DataSourceNodeData, OperatorNodeData } from "../../redux/states/pipelineState";
+import { putCommandStart, putExecution, putPipeline } from "../../services/backendAPI";
 import { getOrganizations, getRepositories } from "../../redux/selectors/apiSelector";
-import { position } from "html2canvas/dist/types/css/property-descriptors/position";
+import { getHandleId } from "./Flow";
 
 export default function PipelineAppBar() {
   const navigate = useNavigate();
@@ -38,22 +37,33 @@ export default function PipelineAppBar() {
 
   const flowData = useSelector(getActiveFlowData)
 
-  const generateJson = () => {
+  const generateJson = async () => {
 
-    console.log(flowData)
+    //console.log(flowData)
+
+    var edges = flowData!.edges.map(edge => {
+      return { sourceHandle: edge.sourceHandle, targetHandle: edge.targetHandle }
+    })
+
+    console.log("copied", edges)
 
     const dataSinks = flowData?.edges.map((edge) => {
       if (edge.data?.filename) {
-        const originalDataSink = flowData.nodes.find(node => node.id === edge.target) as Node<DataSinkNodeData>
+        const newTarget = getHandleId()
+        const egeToModify = edges.find(e => e.sourceHandle == edge.sourceHandle && e.targetHandle == edge.targetHandle)
+        egeToModify!.targetHandle = newTarget
+
+        const originalDataSink = flowData!.nodes.find(node => node.id === edge.target) as Node<DataSinkNodeData>
         return {
           type: originalDataSink?.type,
-          data: {  ...originalDataSink?.data, instantiationData: { resource:{ ...originalDataSink?.data?.instantiationData.repository, name: edge?.data?.filename } }},
-          position: { x: 100, y: 100}, 
-          id: "id1", 
+          data: { ...originalDataSink?.data, templateData: { targetHandles: [{ id: newTarget }], sourceHandles: [] }, instantiationData: { resource: { ...originalDataSink?.data?.instantiationData.repository, name: edge?.data?.filename } } },
+          position: { x: 100, y: 100 },
+          id: "id1",
           width: 100,
           height: 100,
         }
-    }}).filter(node => node !== undefined) as any
+      }
+    }).filter(node => node !== undefined) as any
 
     console.log(JSON.stringify(dataSinks))
 
@@ -61,26 +71,33 @@ export default function PipelineAppBar() {
       name: pipelineName,
       pipeline: {
         nodes: flowData?.nodes?.filter(node => node.type === 'dataSource').map(node => node as Node<DataSourceNodeData>).map(node => {
-          return {  type: node.type, data: { ...node.data, instantiationData :{ resource: { ...node?.data?.instantiationData.resource }, label: "" }},
-          width: 100, height: 100, position: { x: 100, y: 100}, id: "id1", label: "", } as any
+          return {
+            type: node.type, data: { ...node.data, instantiationData: { resource: { ...node?.data?.instantiationData.resource }, label: "" } },
+            width: 100, height: 100, position: { x: 100, y: 100 }, id: "id1", label: "",
+          } as any
         }).concat(
           flowData?.nodes?.filter(node => node.type === 'operator').map(node => node as Node<OperatorNodeData>).map(node => {
-            return {   type: node.type, data: { ...node.data, instantiationData :{ resource: { ...node?.data?.instantiationData.algorithm }, label: "" }},
-            width: 100, height: 100, position: { x: 100, y: 100}, id: "id1", label: "", } as any
+            return {
+              type: node.type, data: { ...node.data, instantiationData: { resource: { ...node?.data?.instantiationData.algorithm }, label: "" } },
+              width: 100, height: 100, position: { x: 100, y: 100 }, id: "id1", label: "",
+            } as any
           })
         ).concat(dataSinks),
-        edges: flowData?.edges.map(edge => {
+        edges: edges.map(edge => {
           return { sourceHandle: edge.sourceHandle, targetHandle: edge.targetHandle }
         })
       }
     }
 
+    console.log(JSON.stringify(requestData))
+    
     const selectedOrg = organizations[0]
     const selectedRepo = repositories.filter(repo => repo.organizationId === selectedOrg.id)[0]
 
-    putPipeline(selectedOrg.id, selectedRepo.id, requestData)
+    const pipelineId = await putPipeline(selectedOrg.id, selectedRepo.id, requestData)
+    const executionId = await putExecution(selectedOrg.id, selectedRepo.id, pipelineId)
+    await putCommandStart(selectedOrg.id, selectedRepo.id, pipelineId, executionId)
 
-    console.log(JSON.stringify(requestData))
   }
 
   return (
@@ -112,3 +129,4 @@ export default function PipelineAppBar() {
     </AppBar>
   )
 }
+
